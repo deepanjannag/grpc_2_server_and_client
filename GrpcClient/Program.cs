2fba74ad8b9f9c2ce1a4dc2892d3229568a90912
Deepanjan Nag <subscriptions@deepanjannag.com>
2022-08-07 22:04:17 -0400

    Initial commit

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "Deepanjan" });
+            
+            Console.WriteLine(reply.Message);
+            Console.ReadLine();
+        }
+    }
+}
