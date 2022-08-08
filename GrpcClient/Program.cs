e717f85af529e9e19e0f6ed679d4482dba3af4fd
Deepanjan Nag <subscriptions@deepanjannag.com>
2022-08-07 23:01:22 -0400

    Added some more elaborate types

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -2,6 +2,7 @@
 using GrpcServer;
 using System;
 using System.Threading.Tasks;
+using static GrpcServer.Customer;
 
 namespace GrpcClient
 {
@@ -9,12 +10,21 @@ namespace GrpcClient
     {
         static async Task Main(string[] args)
         {
-            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            /*var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new Greeter.GreeterClient(channel);
 
             var reply = await client.SayHelloAsync(new HelloRequest { Name = "Deepanjan" });
-            
-            Console.WriteLine(reply.Message);
+
+            Console.WriteLine(reply.Message);*/
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new Customer.CustomerClient(channel);
+
+            var clientRequested = new CustomerLookupModel { Userid = 2 };
+            var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+
+            Console.WriteLine($"{ customer.FirstName}, {customer.LastName}");
+
             Console.ReadLine();
         }
     }
---
+++ GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,57 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        public ILogger<CustomerService> Logger { get; }
+
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            Logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = null;// new CustomerModel();
+
+            if (request.Userid == 1)
+            {
+                output = new CustomerModel
+                {
+                    FirstName = "Wolfgang",
+                    LastName = "Mozart",
+                    EmailAddress = "finance@deepanjannag.com",
+                    IsAlive = false,
+                    Age = 35
+                };
+            }
+            else if (request.Userid == 2)
+            {
+                output = new CustomerModel
+                {
+                    FirstName = "Ludwig",
+                    LastName = "Beethoven",
+                    EmailAddress = "musician@deepanjannag.com",
+                    IsAlive = true,
+                    Age = 57
+                };
+            }
+            else
+            {
+                output = new CustomerModel
+                {
+                    FirstName = "Peter",
+                    LastName = "Tchaikovsky",
+                    EmailAddress = "composer@deepanjannag.com",
+                    IsAlive = false,
+                    Age = 53
+                };
+            }
+
+            return Task.FromResult(output);
+        }
+    }
+}
